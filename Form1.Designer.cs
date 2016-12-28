6cc23803de949f703f1df830691a069bcb5ba503
artoriuspendragon <j540846020@outlook.com>
2016-12-29 00:56:51 +0800

    1
    1

---
+++ Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace WindowsFormsApplication5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label101 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.timertest = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label101
+            // 
+            this.label101.AutoSize = true;
+            this.label101.Location = new System.Drawing.Point(1243, 83);
+            this.label101.Name = "label101";
+            this.label101.Size = new System.Drawing.Size(11, 12);
+            this.label101.TabIndex = 100;
+            this.label101.Text = "1";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1245, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(89, 30);
+            this.button1.TabIndex = 101;
+            this.button1.Text = "开始";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1298, 198);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(57, 48);
+            this.button2.TabIndex = 102;
+            this.button2.Text = "↑";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1232, 266);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(57, 48);
+            this.button3.TabIndex = 103;
+            this.button3.Text = "←";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(1298, 332);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(57, 48);
+            this.button4.TabIndex = 104;
+            this.button4.Text = "↓";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(1369, 266);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(57, 48);
+            this.button5.TabIndex = 105;
+            this.button5.Text = "→";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(1245, 48);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(83, 25);
+            this.button6.TabIndex = 106;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // timertest
+            // 
+            this.timertest.Interval = 500;
+            this.timertest.Tick += new System.EventHandler(this.timertest_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1454, 690);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label101);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.form1_keydown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label101;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Timer timertest;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,265 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication5
+{
+    public partial class Form1 : Form
+    {   const int map_length=20;
+        const int map_height =40;
+        _direction direction =_direction.RIGHT;
+        _direction predirect;
+        public List<PictureBox> board = new List<PictureBox>();
+        public List<point> body = new List<point>();
+        public point head = new point(6, 5);
+        public point apple = new point();
+        public point tail = new point();
+        public point temp = new point();
+        public bool growed = false;
+        int test;
+        
+        public enum _direction
+        {
+            RIGHT = 4,
+            LEFT = 2,
+            DOWN = 1,
+            UP = 3,
+        };
+        public struct point
+        {
+            public int x;
+            public int y;
+         public point(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+
+            }
+        }
+       
+       private void start()
+        {
+            int i;for (i = 0; i < board.Count; i++)
+            { board[i].Image = null; }
+                growed = false;
+            head = new point(6, 5);
+            body.Clear();
+            label101.Text = "";
+            
+            body.Add(new point(5,5));
+            timer1.Enabled = true;
+        }
+        public void stop()
+        { timer1.Enabled = false; }
+     
+        public void grow()
+        {
+            body.Add(new point());
+            growed = false;
+        }
+        public void move() { }
+        private _direction checkdirection()
+        {
+            if (direction == _direction.UP && predirect == _direction.DOWN) return _direction.DOWN;
+            if (direction == _direction.DOWN && predirect == _direction.UP) return _direction.UP;
+            if (direction == _direction.LEFT && predirect == _direction.RIGHT) return _direction.RIGHT;
+            if (direction == _direction.RIGHT && predirect == _direction.LEFT) return _direction.LEFT;
+            return direction;
+
+        }
+        public void play()
+        {
+            int i = 0, c = 0,d=0;
+            temp = head;
+
+            while (true)//苹果位置
+            {
+
+                if (growed)
+                { break; }
+                Random rand = new Random();
+                apple.x = rand.Next(1, map_length);
+                apple.y = rand.Next(1, map_height);
+                growed = true;
+                if (!apple_check())
+                {
+                    break;
+                }
+            }
+            predirect=checkdirection();
+            switch (predirect)
+            {
+                case _direction.DOWN: if (head.y == map_height) head.y = 1; else head.y += 1; break;
+                case _direction.LEFT: if (head.x == 1) head.x = map_length; else head.x -= 1; break;
+                case _direction.UP: if (head.y == 1) head.y = map_height; else head.y -= 1; break;
+                case _direction.RIGHT: if (head.x == map_length) head.x = 1; else head.x += 1; break;
+                default: break;
+
+            }
+            //尾巴
+            if (head.x == apple.x && head.y == apple.y)
+            { grow(); }
+
+            for (i = 0; i < body.Count; i++)
+            {
+                point trans;
+                trans = body[i];
+                body[i] = temp;
+                temp = trans;
+            }
+           d = body[body.Count - 1].x - 1 + map_length * ((body[body.Count - 1].y -1)); //
+            //尾巴
+            //判定
+            for (i = 0; i < body.Count; i++)
+            {
+                if (head.x == body[i].x && head.y == body[i].y)
+                {
+                    Invoke(new MethodInvoker(delegate() { stop(); label101.Text = "you lose"; }));
+
+
+
+                }
+            }
+
+            //判定
+       
+
+            for (i = 0; i < body.Count; i++)
+            {
+                c = map_length * (body[i].y - 1) + body[i].x - 1;
+                board[c].Image = Image.FromFile(Application.StartupPath + "\\snake\\body.jpg");
+            }
+            c = map_length * (apple.y - 1) + apple.x - 1;
+            board[c].Image = Image.FromFile(Application.StartupPath + "\\snake\\body.jpg");
+            c = map_length * (head.y - 1) + head.x - 1;
+            board[c].Image = Image.FromFile(Application.StartupPath + "\\snake\\head.jpg");
+            board[d].Image=null; 
+
+        }
+        public bool apple_check()
+        {
+            int i;
+            for (i = 0; i < body.Count; i++)
+            { if (apple.x == body[i].x && apple.y == body[i].y) return true ; }
+            return false;
+        }
+        public Form1()
+        {
+            
+            
+            InitializeComponent();
+            int i;
+
+            //PictureBox pic1 = new PictureBox();
+            //this.Controls.Add(pic1);
+            //pic1.Location = new Point(0, 0);
+            //pic1.Size = new Size(1000, 560);
+            //Bitmap back = new Bitmap(Application.StartupPath + "\\snake\\background.png");
+            //pic1.Image = back;
+            for (i = 1; i <= map_height * map_length; i++)
+            {
+               
+                
+                PictureBox pic = new PictureBox();
+                pic.Size = new Size(28, 28);
+                float t = (float)(i - 1) / map_length;
+                int tz = (int)t;
+                pic.Location = new Point(28 * ((i - 1) % map_length),28*tz);
+                this.Controls.Add(pic);
+               //pic.BackColor = Color.Transparent; pic.Parent = pic1;
+                pic.BringToFront();
+                board.Add(pic);
+                // pic.Image = Image.FromFile(Application.StartupPath+"\\snake\\beijing.jpg");
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            
+            
+        }
+        public void form1_keydown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.A:
+                        direction = _direction.LEFT; break;
+                case Keys.S:
+                        direction =_direction.DOWN ; break;
+                case Keys.D:
+                        direction =_direction.RIGHT; break;
+                case Keys.W:
+                        direction = _direction.UP; break;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            start();
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (head.y != body[0].y - 1)
+                direction =_direction.UP;
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (head.x != body[0].x - 1)
+                direction = _direction.LEFT;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (head.y != body[0].y + 1)
+                direction = _direction.DOWN;
+
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (head.x != body[0].x + 1)
+                direction = _direction.RIGHT;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            System.Threading.Thread t = new System.Threading.Thread(play);
+            t.Start();
+            
+            
+               
+            
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            timertest.Enabled = true;
+        }
+
+        private void timertest_Tick(object sender, EventArgs e)
+        {   board[test].BackColor = System.Drawing.Color.Black;
+             test++;
+             if (test == 100) { timertest.Enabled = false; test = 0; }
+        }
+
+      
+
+    
+      
+    }
+}
